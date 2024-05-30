c5a062104eb3c23029cf820f66f944c57f393caf
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2024-05-30 14:18:02 +0700

    Update draft UI, add tailwindcss

--- DiamondShop.WebApp/src/pages/Products/index.tsx
+++ DiamondShop.WebApp/src/pages/Products/index.tsx
@@ -16,7 +16,25 @@ const ProductsPage = (props: Props) => {
                 // alignItems: "center",
                 // justifyContent: "center",
             }}>
-                <div className="card_list">
+                <div className="filter_bar flex gap-3">
+                    <select name="" id="" 
+                            className="material_option border border-black rounded-sm p-2">
+                        <option value="" defaultChecked>Chất liệu</option>
+                        <option value="">Vàng</option>
+                        <option value="">Bạc</option>
+                    </select>
+                    <select name="" id="" 
+                    className="diamonds_option border border-black rounded-sm p-2">
+                        <option value="" defaultChecked>Kim cương đính kèm</option>
+                        <option value="">Kim cương LUCKY START</option>
+                    </select>
+                    <div className="search_bar">
+                        <input type="search" 
+                                className='border border-black rounded-sm p-2'
+                                 placeholder='Tìm kiếm sản phẩm...' />
+                    </div>
+                </div>
+                <div className="card_list mt-20">
                     <div className="card">
                         <div className="card_img">
                             <img src="https://locphuc.com.vn/Content/Images/Product/San-pham-lan-22/bong-tai-kim-cuong-vang-trang-18k-DFH0149E-D02200122-g3.jpg" className='' alt="" />
