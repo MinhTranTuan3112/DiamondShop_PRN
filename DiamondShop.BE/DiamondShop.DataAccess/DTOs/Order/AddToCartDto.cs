a376fed85fbbd8f861ddf6947c2d4120da1ed8f8
MinhTranTuan12 <minhttse172842@fpt.edu.vn>
2024-05-31 13:30:49 +0700

    Add quantitiy validation

--- DiamondShop.BE/DiamondShop.DataAccess/DTOs/Order/AddToCartDto.cs
+++ DiamondShop.BE/DiamondShop.DataAccess/DTOs/Order/AddToCartDto.cs
@@ -9,6 +9,7 @@ namespace DiamondShop.DataAccess.DTOs.Order
     public class AddToCartDto
     {
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
         public required int Quantity { get; set; }
 
         [Required]
