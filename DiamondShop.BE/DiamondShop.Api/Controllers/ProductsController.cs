9132e23e349cc70fe2efca4856ccf9fc55331e36
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2024-06-11 13:44:29 +0700

    Fix endpoint method name

--- DiamondShop.BE/DiamondShop.Api/Controllers/ProductsController.cs
+++ DiamondShop.BE/DiamondShop.Api/Controllers/ProductsController.cs
@@ -21,9 +21,9 @@ namespace DiamondShop.Api.Controllers
         }
         [HttpPost]
         // [Authorize(Roles = "SalesStaff, sales-staff")]
-        public async Task<ActionResult<GetProductIdDto>> CreateDiamond([FromForm] CreateProductDto createProductDto)
+        public async Task<ActionResult<GetProductIdDto>> CreateProduct([FromForm] CreateProductDto createProductDto)
         {
-            return Created(nameof(CreateDiamond), await _serviceFactory.GetProductService().CreateProduct(createProductDto));
+            return Created(nameof(CreateProduct), await _serviceFactory.GetProductService().CreateProduct(createProductDto));
         }
         [HttpGet]
         public async Task<ActionResult<PagedResult<GetProductInPagedResultDto>>> GetPagedProducts([FromQuery] QueryProductDto queryProductDto)
