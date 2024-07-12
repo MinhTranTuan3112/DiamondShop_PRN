923ab864cae0e954734ca876f68563d0bbd60e59
Châu Thiên Ân <chauthienan01072002@gmail.com>
2024-07-12 13:49:21 +0700

    fix update product

--- DiamondShop.BE/DiamondShop.DataAccess/DTOs/Product/UpdateProductDto.cs
+++ DiamondShop.BE/DiamondShop.DataAccess/DTOs/Product/UpdateProductDto.cs
@@ -6,15 +6,15 @@ namespace DiamondShop.DataAccess.DTOs.Product;
 
 public class UpdateProductDto
 {
-    public required string Name { get; set; }
+    public string? Name { get; set; }
     [EnumDataType(typeof(ProductType))]
-    public required string Type { get; set; }
-    public required string Material { get; set; }
-    public bool Gender { get; set; }
-    public decimal Price { get; set; }
-    public int Point { get; set; }
-    public int Quantity { get; set; }
-    public int WarrantyPeriod { get; set; }
-    public Guid CategoryId { get; set; } 
+    public string? Type { get; set; }
+    public string? Material { get; set; }
+    public bool? Gender { get; set; }
+    public decimal? Price { get; set; }
+    public int? Point { get; set; }
+    public int? Quantity { get; set; }
+    public int? WarrantyPeriod { get; set; }
+    public Guid? CategoryId { get; set; } 
     public List<IFormFile> ProductPicture { get; set; } = [];
 }
