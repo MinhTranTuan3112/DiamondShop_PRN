01fd642b9418c396f6824fb5b5000628a469cc26
Châu Thiên Ân <chauthienan01072002@gmail.com>
2024-07-15 21:24:39 +0700

    Update Paging Certificate

--- DiamondShop.BE/DiamondShop.DataAccess/DTOs/Certificate/QueryCertificateDto.cs
+++ DiamondShop.BE/DiamondShop.DataAccess/DTOs/Certificate/QueryCertificateDto.cs
@@ -1,13 +1,28 @@
 ﻿using DiamondShop.DataAccess.DTOs.Query;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiamondShop.DataAccess.DTOs.Certificate;
 
 public class QueryCertificateDto
 {
-    public QueryDto QueryDto { get; set; } = new QueryDto();
+    [FromQuery(Name = "pageNumber")]
+    public int PageNumber { get; set; } = 1;
+
+    [FromQuery(Name = "pageSize")]
+    public int PageSize { get; set; } = 10;
+
+    [FromQuery(Name = "sortColumn")]
+    public string SortColumn { get; set; } = "id";
+
+    [FromQuery(Name = "orderByDesc")]
+    public bool OrderByDesc { get; set; } = false;
+    [FromQuery(Name = "reportNumber")]
     public string? ReportNumber { get; set; }
+    [FromQuery(Name = "startDateOfIssue")]
     public DateTime StartDateOfIssue { get; set; }
+    [FromQuery(Name = "endDateOfIssue")]
     public DateTime EndDateOfIssue { get; set; }
+    [FromQuery(Name = "diamondId")]
     public Guid? DiamondId { get; set; }
     
 }
